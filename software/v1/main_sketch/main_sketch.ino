33bd6de44a3ea118350ebf8ac6f912317bb35dd9
Kelvin Nicholson <kelvin@kelvinism.com>
2018-10-01 16:34:20 +1000

    Added n-ch mosfet to kill servo power after moved back to 'default' position

--- software/v1/main_sketch/main_sketch.ino
+++ software/v1/main_sketch/main_sketch.ino
@@ -2,44 +2,76 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>  
 
+int powerControl = 3; // D3 pin connected to MOSFET gate
 int bluetoothTx = 6;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 7;  // RX-I pin of bluetooth mate, Arduino D3
+int servoPin = 9;     // D9 pin connected to servo orange wire
 
-Servo myservo;  // create servo object to control a servo
+Servo servo;          // create servo object to control a servo
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
-const unsigned long period = 3000;  //the value is a number of milliseconds
-unsigned long lastPush;  //some global variables available anywhere in the program
-unsigned long currentMillis;
-char bluetoothCmd = 'A';
+const unsigned long period = 5000;  // Push button wait period in milliseconds
+unsigned long lastPush;             // Store last push time
+unsigned long currentMillis;        // Store current time
+char bluetoothCmd = 'A';            // Read a char via bluetooth
 
 void setup() {
-  // put your setup code here, to run once:
+  // The below code only runs once
   Serial.begin(9600);  // Begin the serial monitor at 9600bps
   Serial.println("Running Setup Block");
+  
+  pinMode(powerControl, OUTPUT); // powerControl connects to base pin of MOSFET
+  
   lastPush = millis();
-  delay(1000); // Wait for bluetooth to start upZ
+  delay(1000);              // Wait for bluetooth to start upZ
   bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
-  bluetooth.print("$");  // Print three times individually
+  bluetooth.print("$");     // Print three times individually
   bluetooth.print("$");
-  bluetooth.print("$");  // Enter command mode
-  delay(100);  // Short delay, wait for the Mate to send back CMD
+  bluetooth.print("$");     // Enter command mode
+  
+  delay(100);               // Short delay, wait for the Mate to send back CMD
+  
   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
-  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
-  bluetooth.begin(9600);  // Start bluetooth serial at 9600
-  delay(100);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(165); // Move to default position
+                                  // 115200 can be too fast at times for 
+                                  // NewSoftSerial to relay the data reliably
+  bluetooth.begin(9600);          // Start bluetooth serial at 9600
+  
+  delay(100);               // Short delay, wait for Mat to change baud rate
 
+  Serial.println("Turning ON Mosfet");
+  digitalWrite(powerControl, HIGH); // Turn on MOSFET
+  servo.attach(servoPin);           // Attach servo
+  servo.write(165);                 // Move to default position
+
+  delay(500);               // Short delay to let servo return to default position
+
+  Serial.println("Turning OFF Mosfet");
+  digitalWrite(powerControl, LOW);
+  servo.detach();
   
 }
 
 void push_button() {
 
-  myservo.write(10);                  // Moves servo forward to push button
-  delay(500);                        // Wait for 500ms
-  myservo.write(165);                 // Move back
+  Serial.println("Attaching Servo And Turning ON Mosfet");
+  servo.attach(servoPin);
+  digitalWrite(powerControl, HIGH);
+  Serial.println("Servo Power is ON");
+  delay(200);
 
+  Serial.println("Pushing Button");
+  servo.write(10);                  // Moves servo forward to push button
+  delay(500);                       // Wait for 500ms
+  servo.write(165);                 // Move back
+
+  delay(1000);                      // Short delay to let servo return to default position
+  
+  Serial.println("Attaching Servo And Turning OFF Mosfet");
+   digitalWrite(powerControl, LOW);
+   servo.detach();
+   Serial.println("Servo detached");
+
+   delay(1000);                     // Let servo detach
 }
 
 void loop() {
